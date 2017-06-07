27f60013f741126aa9e24674774e2be836dd8ab2
leexin <cnbdlixin@163.com>
2017-06-07 17:09:56 +0800

    阶段3

--- python/DemoHttp.py
+++ python/DemoHttp.py
@@ -4,26 +4,34 @@
 import tornado.ioloop
 import tornado.web
 import tornado.httpclient
+from python.compy.Demohandler import DemoHandler
+from python.coreseek.Searchhandler import Searchhandler
 class MainHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         http = tornado.httpclient.AsyncHTTPClient()
-        response = yield http.fetch("htt://www.baidu.com")
+        response = yield http.fetch("http://www.baidu.com")
         self.write(response.body)
     def post(self):
         key = self.get_argument("key")
         self.set_status(300,reason=None)
+        key = key+"1"
         self.write(key)
 def make_app():
     return tornado.web.Application([
         (r"/",MainHandler)
         ,
-    ]
+        (r"/demo",DemoHandler)
+        ,
+        (r"/search",Searchhandler)
+        ,
+    ],
+        debug= True
 
     )
 def main():
     app = make_app()
-    app.listen(8889)
+    app.listen(8887)
     tornado.ioloop.IOLoop.current().start()
 
 if __name__=="__main__":
